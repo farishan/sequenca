bb186eb42312304664c7db6daa3e5b9156578ef3
Faris Hanugraha <farishanugraha@gmail.com>
2018-08-05 23:16:57 +0700

    init

--- main.js
+++ main.js
@@ -1,16 +1,49 @@
-console.log(Vue)
-
 var app = new Vue({
 	el: '#app',
 	data: function(){
 		return {
-
+			listLength: 0,
+			min: 0,
+			max: 0,
+			list: []
 		}
 	},
 	mounted(){
 
 	},
 	methods: {
-
+		generate(){
+			if(this.listLength<100){
+				if(this.listLength%2==0){
+					this.listLength++;
+				}
+				this.list = oasbtn(this.min, this.max, this.listLength);
+				console.log(this.list)
+			}
+		}
 	}
 });
+
+// oasbtn = Odd Arithmetic Sequence Between Two Number
+function oasbtn(a, b, l){
+	var a = parseInt(a);
+	var b = parseInt(b);
+	var l = parseInt(l);
+	var r = [];
+	var d = b-((b+a)/2);
+	if(l%2 == 0){
+		r = false;
+	}else{
+		var k = 0;
+		for (var i = 0; i <= l; i++) {
+			if(i%2==1 && i>3){
+				k++;
+			}
+		}
+		var nd = d/(k+1);
+		for (var i = 0; i < l; i++) {
+			r.push(a + nd*i);
+		}
+	}
+	return r;
+}
